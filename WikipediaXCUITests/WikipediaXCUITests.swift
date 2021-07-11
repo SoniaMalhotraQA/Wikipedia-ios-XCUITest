84db8bbf9d60eb286d5f5cb0e18314944c489550
SoniaMalhotraQA <87262553+SoniaMalhotraQA@users.noreply.github.com>
2021-07-11 21:51:53 +0200

    Added comments

--- WikipediaXCUITests/WikipediaXCUITests.swift
+++ WikipediaXCUITests/WikipediaXCUITests.swift
@@ -3,21 +3,23 @@
 import XCTest
 
 class WikipediaXCUITests: BaseXCUITest {
-
+    
+    //Test to check existance of Search field
     func testSearchFieldExists() throws {
         let searchField : XCUIElement = WikipediaScreen.searchWikipediaField.element
         XCTAssertTrue(searchField.exists)
         }
     
+    //Test to check search query results existance    
     func testSearchQuery() throws{
         let searchText: String = "Weather"
-        //let searchField = BaseXCUITest.getSearchField()
         let searchField = WikipediaScreen.searchWikipediaField.element
         searchField.tap()
         searchField.typeText(searchText)
-        XCTAssertTrue( BaseXCUITest.getResultCollection().staticTexts.element(boundBy: 0).exists)
+        XCTAssertTrue( BaseXCUITest.getResultCollection().staticTexts.element(boundBy: 2).exists)
         }
     
+    //Test to check Search history data
     func testSearchHistory() throws {
         let searchText: String = "Berlin"
         let searchField = WikipediaScreen.searchWikipediaField.element
@@ -33,6 +35,7 @@ class WikipediaXCUITests: BaseXCUITest {
         XCTAssertEqual(BaseXCUITest.getResultCollection().staticTexts.element(boundBy: 2).label, searchText)
         }
     
+    //Test to clear Search history data
     func testClearSearch() throws {
         let searchText: String = "Weather tomorrow"
         let searchField = WikipediaScreen.searchWikipediaField.element
@@ -47,7 +50,7 @@ class WikipediaXCUITests: BaseXCUITest {
         XCTAssertTrue(WikipediaScreen.noRecentSearchesYet.element.waitForExistence(timeout: 1))
         }
 
-
+//    Predefined Test to check performance of application
 //    func testLaunchPerformance() throws {
 //        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
 //            // This measures how long it takes to launch your application.
